44aecc2051fcbcc276614ccadb9f264906b30197
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-11-12 19:03:57 -0300

    Add logic to format flight date's field in RFC3339

--- src/main/kotlin/edu/fiuba/reservations/delivery/dto/response/ReservationDTO.kt
+++ src/main/kotlin/edu/fiuba/reservations/delivery/dto/response/ReservationDTO.kt
@@ -1,10 +1,14 @@
 package edu.fiuba.reservations.delivery.dto.response
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import edu.fiuba.reservations.domain.enums.AirlineCode
+import edu.fiuba.reservations.utils.DATE_TIME_ZONE
+import edu.fiuba.reservations.utils.RFC3339PATTERN
 import java.time.ZonedDateTime
 
 data class ReservationDTO(
     val id: String,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = RFC3339PATTERN, timezone = DATE_TIME_ZONE)
     val flightDate: ZonedDateTime,
     val airline: AirlineCode
 )
