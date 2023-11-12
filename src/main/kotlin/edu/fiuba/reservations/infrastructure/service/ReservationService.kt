6e305e1468921419a739c0906125333c83b6ed9d
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-11-12 18:59:14 -0300

    Update repository's reference with its persistence's class

--- src/main/kotlin/edu/fiuba/reservations/infrastructure/service/ReservationService.kt
+++ src/main/kotlin/edu/fiuba/reservations/infrastructure/service/ReservationService.kt
@@ -1,14 +1,14 @@
 package edu.fiuba.reservations.infrastructure.service
 
 import edu.fiuba.reservations.domain.entity.Reservation
-import edu.fiuba.reservations.infrastructure.client.persistence.repository.ReservationRepository
+import edu.fiuba.reservations.infrastructure.client.persistence.ReservationPersistence
 import org.springframework.stereotype.Service
 
 @Service
 class ReservationService(
-    private val reservationRepository: ReservationRepository
+    private val reservationPersistence: ReservationPersistence
 ) {
     fun getReservation(id: String): Reservation {
-        return reservationRepository.get(id)
+        return reservationPersistence.getReservation(id)
     }
 }
