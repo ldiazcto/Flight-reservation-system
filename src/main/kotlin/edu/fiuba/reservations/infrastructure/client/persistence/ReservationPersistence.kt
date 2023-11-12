d673516166863cdef8e3ebf94c41f6ff70ff64ec
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-11-12 18:55:55 -0300

    Add logic to map from reservation's entity to business class

--- src/main/kotlin/edu/fiuba/reservations/infrastructure/client/persistence/ReservationPersistence.kt
+++ src/main/kotlin/edu/fiuba/reservations/infrastructure/client/persistence/ReservationPersistence.kt
@@ -3,12 +3,20 @@ package edu.fiuba.reservations.infrastructure.client.persistence
 import com.google.cloud.firestore.CollectionReference
 import com.google.cloud.firestore.Firestore
 import edu.fiuba.reservations.domain.entity.Reservation
+import edu.fiuba.reservations.infrastructure.client.persistence.entity.ReservationEntity
+import edu.fiuba.reservations.infrastructure.client.persistence.mapper.ReservationEntityMapper
 import edu.fiuba.reservations.infrastructure.client.persistence.repository.GenericRepositoryImpl
 import edu.fiuba.reservations.infrastructure.client.persistence.repository.ReservationRepository
 
 class ReservationPersistence(
     private val firestore: Firestore
-) : ReservationRepository, GenericRepositoryImpl<Reservation>(Reservation::class.java) {
+) : ReservationRepository, GenericRepositoryImpl<ReservationEntity>(ReservationEntity::class.java) {
+
+    fun getReservation(id: String): Reservation {
+        return ReservationEntityMapper.toReservationDomain(
+            get(id)
+        )
+    }
 
     override fun getCollection(): CollectionReference {
         return firestore.collection("reservations")
