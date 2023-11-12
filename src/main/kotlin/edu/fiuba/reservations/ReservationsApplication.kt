8a4952ca5185c845d64475b8391928468c73c6dd
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-11-12 17:30:16 -0300

    Add method to get logger in any part of the project

--- src/main/kotlin/edu/fiuba/reservations/ReservationsApplication.kt
+++ src/main/kotlin/edu/fiuba/reservations/ReservationsApplication.kt
@@ -1,5 +1,7 @@
 package edu.fiuba.reservations
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
@@ -10,4 +12,8 @@ class ReservationsApplication
 
 fun main(args: Array<String>) {
 	runApplication<ReservationsApplication>(*args)
+}
+
+fun <R : Any> R.logger(): Lazy<Logger> {
+	return lazy { LoggerFactory.getLogger(this.javaClass) }
 }
