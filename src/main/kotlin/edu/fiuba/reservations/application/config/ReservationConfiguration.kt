a45a1b609259e3414a0e22721a8e6026bd04dc10
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-11-12 23:07:55 -0300

    Add logic to inject collection's name in reservation's config

--- src/main/kotlin/edu/fiuba/reservations/application/config/ReservationConfiguration.kt
+++ src/main/kotlin/edu/fiuba/reservations/application/config/ReservationConfiguration.kt
@@ -4,6 +4,7 @@ import com.google.cloud.firestore.Firestore
 import edu.fiuba.reservations.delivery.controller.ReservationController
 import edu.fiuba.reservations.infrastructure.client.persistence.ReservationPersistence
 import edu.fiuba.reservations.infrastructure.service.ReservationService
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -20,9 +21,11 @@ class ReservationConfiguration {
 
     @Bean
     fun reservationPersistence(
+        @Value("\${data.firebase.collections.reservation.name}") collectionName: String,
         firestore: Firestore
     ): ReservationPersistence {
         return ReservationPersistence(
+            collectionName,
             firestore
         )
     }
