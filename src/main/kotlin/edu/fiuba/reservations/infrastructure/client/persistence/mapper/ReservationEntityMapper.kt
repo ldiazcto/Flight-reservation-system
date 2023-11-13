90d7d17e7502103cbf825551b5792d0a331506b1
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-11-13 14:54:09 -0300

    Update old references of constants with new ones

--- src/main/kotlin/edu/fiuba/reservations/infrastructure/client/persistence/mapper/ReservationEntityMapper.kt
+++ src/main/kotlin/edu/fiuba/reservations/infrastructure/client/persistence/mapper/ReservationEntityMapper.kt
@@ -4,14 +4,14 @@ import edu.fiuba.reservations.domain.entity.Reservation
 import edu.fiuba.reservations.domain.enums.AirlineCode
 import edu.fiuba.reservations.domain.enums.fromCode
 import edu.fiuba.reservations.infrastructure.client.persistence.entity.ReservationEntity
-import edu.fiuba.reservations.utils.YYYY_MM_DD_HH_MM_SS_SSSS
+import edu.fiuba.reservations.utils.Constants.DATETIME_PATTERN
 import edu.fiuba.reservations.utils.toDateFromPatternWithHours
 
 object ReservationEntityMapper {
     fun toReservationDomain(entity: ReservationEntity): Reservation {
         return Reservation(
             id = entity.id,
-            flightDate = entity.flightDate.toDateFromPatternWithHours(YYYY_MM_DD_HH_MM_SS_SSSS)!!,
+            flightDate = entity.flightDate.toDateFromPatternWithHours(DATETIME_PATTERN)!!,
             airline = AirlineCode.fromCode(entity.airline)!!
         )
     }
