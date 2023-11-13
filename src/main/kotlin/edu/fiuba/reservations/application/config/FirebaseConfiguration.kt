2740fc867b4a5331a77e6192e402dd031ca58085
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-11-12 23:06:12 -0300

    Add logic to inject firebase's path in configuration

--- src/main/kotlin/edu/fiuba/reservations/application/config/FirebaseConfiguration.kt
+++ src/main/kotlin/edu/fiuba/reservations/application/config/FirebaseConfiguration.kt
@@ -5,15 +5,19 @@ import com.google.cloud.firestore.Firestore
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.cloud.FirestoreClient
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.io.FileInputStream
 
 @Configuration
 class FirebaseConfiguration {
+    @Value("\${data.firebase.path}")
+    private lateinit var firebaseAccountInfoPath: String
+
     @Bean
     fun firestore(): Firestore {
-        val serviceAccount = FileInputStream("./src/main/resources/firebase-account-info.json")
+        val serviceAccount = FileInputStream(firebaseAccountInfoPath)
         val options = FirebaseOptions.builder()
             .setCredentials(GoogleCredentials.fromStream(serviceAccount)).build()
         val firebaseApp = FirebaseApp.initializeApp(options)
