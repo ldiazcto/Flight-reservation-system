87500fea7b73e1125ce7df2367670569ffce4bd1
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-11-20 18:34:24 -0300

    Add builder for flight criteria's DTO

---
+++ src/main/kotlin/edu/fiuba/reservations/delivery/dto/request/builder/FlightCriteriaDTOBuilder.kt
@@ -0,0 +1,25 @@
+package edu.fiuba.reservations.delivery.dto.request.builder
+
+import edu.fiuba.reservations.application.exception.ExceptionCode
+import edu.fiuba.reservations.delivery.dto.request.FlightCriteriaDTO
+import edu.fiuba.reservations.delivery.validator.FlightCriteriaDTOValidator
+import edu.fiuba.reservations.domain.entity.Error
+import edu.fiuba.reservations.domain.exception.BadRequestException
+
+class FlightCriteriaDTOBuilder(
+    private val validator: FlightCriteriaDTOValidator
+) {
+    fun build(data: FlightCriteriaDTO): FlightCriteriaDTO {
+        val exceptions = validator.validate(data, ArrayList())
+
+        if (exceptions.isEmpty()) {
+            return FlightCriteriaDTO(data)
+        } else {
+            throw BadRequestException(
+                ExceptionCode.INVALID_FLIGHT_CRITERIA.getMessage(),
+                ExceptionCode.INVALID_FLIGHT_CRITERIA.getCode(),
+                exceptions.map { Error(it) }
+            )
+        }
+    }
+}
