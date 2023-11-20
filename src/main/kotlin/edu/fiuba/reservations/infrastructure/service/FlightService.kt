f08a132670808fdb4bc86cd9d7514cf70fa96cf4
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-11-20 18:33:25 -0300

    Add flight's service

---
+++ src/main/kotlin/edu/fiuba/reservations/infrastructure/service/FlightService.kt
@@ -0,0 +1,19 @@
+package edu.fiuba.reservations.infrastructure.service
+
+import edu.fiuba.reservations.delivery.dto.request.FlightCriteriaDTO
+import edu.fiuba.reservations.domain.entity.FlightSearch
+import edu.fiuba.reservations.domain.entity.builder.FlightCriteriaBuilder
+import edu.fiuba.reservations.domain.validator.FlightCriteriaValidator
+import edu.fiuba.reservations.infrastructure.client.file.FlightFileManager
+import org.springframework.stereotype.Service
+
+@Service
+class FlightService(
+    private val flightFileManager: FlightFileManager
+) {
+    fun getFlights(flightCriteria: FlightCriteriaDTO): List<FlightSearch> {
+        val validatedFlightCriteria = FlightCriteriaBuilder(FlightCriteriaValidator()).build(flightCriteria)
+
+        return flightFileManager.getFlights(validatedFlightCriteria)
+    }
+}
