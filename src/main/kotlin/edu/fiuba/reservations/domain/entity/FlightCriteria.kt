74f0de2fd9133cf6c27e6cd0db5b2de9a5c057e0
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-11-27 01:02:44 -0300

    Update type of fields and add origin and destination fields

--- src/main/kotlin/edu/fiuba/reservations/domain/entity/FlightCriteria.kt
+++ src/main/kotlin/edu/fiuba/reservations/domain/entity/FlightCriteria.kt
@@ -2,19 +2,24 @@ package edu.fiuba.reservations.domain.entity
 
 import edu.fiuba.reservations.delivery.dto.request.FlightCriteriaDTO
 import edu.fiuba.reservations.domain.enums.AirlineCode
+import edu.fiuba.reservations.domain.enums.AirportCode
 import edu.fiuba.reservations.domain.enums.fromValue
 import edu.fiuba.reservations.utils.Constants.DATE_PATTERN
 import edu.fiuba.reservations.utils.toDateFromPatternWithoutHours
 import java.time.ZonedDateTime
 
 data class FlightCriteria(
-    val airline: AirlineCode?,
-    val from: ZonedDateTime?,
-    val to: ZonedDateTime?
+    val airline: AirlineCode,
+    val origin: AirportCode,
+    val destination: AirportCode,
+    val from: ZonedDateTime,
+    val to: ZonedDateTime
 ) {
     constructor(entity: FlightCriteriaDTO) : this(
-        airline = entity.airline?.let { AirlineCode.fromValue(it) },
-        from = entity.from?.toDateFromPatternWithoutHours(DATE_PATTERN),
-        to = entity.to?.toDateFromPatternWithoutHours(DATE_PATTERN)?.plusDays(1)?.minusNanos(1)
+        airline = entity.airline?.let { AirlineCode.fromValue(it) }!!,
+        origin = entity.origin?.let { AirportCode.fromValue(it) }!!,
+        destination = entity.destination?.let { AirportCode.fromValue(it) }!!,
+        from = entity.from?.toDateFromPatternWithoutHours(DATE_PATTERN)!!,
+        to = entity.to?.toDateFromPatternWithoutHours(DATE_PATTERN)?.plusDays(1)?.minusNanos(1)!!
     )
 }
