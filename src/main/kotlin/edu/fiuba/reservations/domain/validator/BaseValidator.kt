ab6e2be98dbb397afd11148f2d707ee074fc326a
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-11-27 00:57:37 -0300

    Add destination's validator initialization

--- src/main/kotlin/edu/fiuba/reservations/domain/validator/BaseValidator.kt
+++ src/main/kotlin/edu/fiuba/reservations/domain/validator/BaseValidator.kt
@@ -1,5 +1,6 @@
 package edu.fiuba.reservations.domain.validator
 
 abstract class BaseValidator {
+    protected val destinationValidator = DestinationValidator()
     protected val dateValidator = DateValidator()
 }
