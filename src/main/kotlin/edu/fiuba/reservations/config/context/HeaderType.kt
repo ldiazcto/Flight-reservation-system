c4861c49bb27409424589d3c10c39d62b9ec256e
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-12-03 02:01:42 -0300

    Add better syntax

--- src/main/kotlin/edu/fiuba/reservations/config/context/HeaderType.kt
+++ src/main/kotlin/edu/fiuba/reservations/config/context/HeaderType.kt
@@ -1,5 +1,7 @@
 package edu.fiuba.reservations.config.context
 
-enum class HeaderType(val label: String) {
+enum class HeaderType(
+    val label: String
+) {
     REQUEST_ID("X-FIU-CorrelationId");
 }
