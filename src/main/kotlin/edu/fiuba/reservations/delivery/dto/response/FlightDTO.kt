f0be435d9622859b7d53e6d022c3b3d7b93980e5
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-12-02 22:30:06 -0300

    Fix date's format in DTOs

--- src/main/kotlin/edu/fiuba/reservations/delivery/dto/response/FlightDTO.kt
+++ src/main/kotlin/edu/fiuba/reservations/delivery/dto/response/FlightDTO.kt
@@ -1,11 +1,14 @@
 package edu.fiuba.reservations.delivery.dto.response
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import edu.fiuba.reservations.domain.entity.Flight
 import edu.fiuba.reservations.domain.enums.AirlineCode
 import edu.fiuba.reservations.domain.enums.AirportCode
 import edu.fiuba.reservations.domain.enums.CountryCode
 import edu.fiuba.reservations.domain.enums.FlightType
 import edu.fiuba.reservations.domain.enums.StateCode
+import edu.fiuba.reservations.utils.Constants.ARGENTINE_TIME_ZONE
+import edu.fiuba.reservations.utils.Constants.DATETIME_RFC3339_PATTERN
 import java.math.BigDecimal
 import java.time.ZonedDateTime
 
@@ -21,9 +24,13 @@ data class FlightDTO(
     val destinationCity: String,
     val destinationState: StateCode,
     val destinationCountry: CountryCode,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATETIME_RFC3339_PATTERN, timezone = ARGENTINE_TIME_ZONE)
     val plannedDepartureTime: ZonedDateTime,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATETIME_RFC3339_PATTERN, timezone = ARGENTINE_TIME_ZONE)
     val realDepartureTime: ZonedDateTime,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATETIME_RFC3339_PATTERN, timezone = ARGENTINE_TIME_ZONE)
     val plannedArrivalTime: ZonedDateTime,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATETIME_RFC3339_PATTERN, timezone = ARGENTINE_TIME_ZONE)
     val realArrivalTime: ZonedDateTime,
     val price: BigDecimal
 ) {
--- src/main/kotlin/edu/fiuba/reservations/delivery/dto/response/FlightSearchListDTO.kt
+++ src/main/kotlin/edu/fiuba/reservations/delivery/dto/response/FlightSearchListDTO.kt
@@ -1,8 +1,10 @@
 package edu.fiuba.reservations.delivery.dto.response
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import edu.fiuba.reservations.domain.entity.FlightSearch
 import edu.fiuba.reservations.domain.enums.AirlineCode
 import edu.fiuba.reservations.domain.enums.AirportCode
+import edu.fiuba.reservations.utils.Constants
 import java.math.BigDecimal
 import java.time.ZonedDateTime
 
@@ -14,7 +16,9 @@ data class FlightSearchListDTO(
         val airline: AirlineCode,
         val originAirport: AirportCode,
         val destinationAirport: AirportCode,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Constants.DATETIME_RFC3339_PATTERN, timezone = Constants.ARGENTINE_TIME_ZONE)
         val plannedDepartureTime: ZonedDateTime,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Constants.DATETIME_RFC3339_PATTERN, timezone = Constants.ARGENTINE_TIME_ZONE)
         val plannedArrivalTime: ZonedDateTime,
         val price: BigDecimal
     ) {
