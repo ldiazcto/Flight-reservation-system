19bd24bd103e48a09cd531f19dd7ec1af43818ea
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-12-02 21:49:04 -0300

    Add departure and arrival fields

--- src/main/kotlin/edu/fiuba/reservations/domain/entity/FlightSearch.kt
+++ src/main/kotlin/edu/fiuba/reservations/domain/entity/FlightSearch.kt
@@ -2,6 +2,10 @@ package edu.fiuba.reservations.domain.entity
 
 import edu.fiuba.reservations.domain.enums.AirlineCode
 import edu.fiuba.reservations.domain.enums.AirportCode
+import edu.fiuba.reservations.domain.enums.fromPartialValue
+import edu.fiuba.reservations.domain.enums.fromValue
+import edu.fiuba.reservations.utils.toCustomBigDecimal
+import org.apache.commons.csv.CSVRecord
 import java.math.BigDecimal
 import java.time.ZonedDateTime
 
@@ -10,7 +14,17 @@ data class FlightSearch(
     val airline: AirlineCode,
     val originAirport: AirportCode,
     val destinationAirport: AirportCode,
-    val departureTime: ZonedDateTime,
-    val arrivalTime: ZonedDateTime,
+    val plannedDepartureTime: ZonedDateTime,
+    val plannedArrivalTime: ZonedDateTime,
     val price: BigDecimal
-)
+) {
+    constructor(entity: CSVRecord) : this(
+        id = entity.get("flight_id"),
+        airline = AirlineCode.fromValue(entity.get("airline").replace(" ", "_"))!!,
+        originAirport = AirportCode.fromPartialValue(entity.get("origin_airport"))!!,
+        destinationAirport = AirportCode.fromPartialValue(entity.get("destination_airport"))!!,
+        plannedDepartureTime = ZonedDateTime.parse(entity.get("planned_departure")),
+        plannedArrivalTime = ZonedDateTime.parse(entity.get("planned_arrival")),
+        price = entity.get("price").toCustomBigDecimal()
+    )
+}
