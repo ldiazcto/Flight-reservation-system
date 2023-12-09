3d196b06b45ba6f4ab9ba7d36e432476b9c8b911
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-12-09 17:14:27 -0300

    Add missing fields in reservation's entity

--- src/main/kotlin/edu/fiuba/reservations/infrastructure/client/persistence/entity/ReservationEntity.kt
+++ src/main/kotlin/edu/fiuba/reservations/infrastructure/client/persistence/entity/ReservationEntity.kt
@@ -1,7 +1,15 @@
 package edu.fiuba.reservations.infrastructure.client.persistence.entity
 
+import java.math.BigDecimal
+import java.math.BigInteger.ZERO
+
 data class ReservationEntity(
     var id: String = String(),
     val flightDate: String = String(),
-    val airline: String = String()
+    val airline: String = String(),
+    val origin: String = String(),
+    val destination: String = String(),
+    val passengersQuantity: Int = ZERO.toInt(),
+    val totalPrice: BigDecimal = BigDecimal.ZERO,
+    val customer: CustomerEntity = CustomerEntity()
 )
