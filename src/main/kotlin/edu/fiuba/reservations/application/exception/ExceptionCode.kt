059ab822116de2fb81c3d0f71f7cc7d57c8b7580
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-12-11 15:24:25 -0300

    Add exceptions for invalid reservation id and reservation body

--- src/main/kotlin/edu/fiuba/reservations/application/exception/ExceptionCode.kt
+++ src/main/kotlin/edu/fiuba/reservations/application/exception/ExceptionCode.kt
@@ -9,6 +9,8 @@ enum class ExceptionCode(
     FLIGHT_NOT_FOUND("flight.not.found", "The flight was not found"),
     INVALID_FLIGHT_CRITERIA("error.params", "The flight criteria provided is not valid"),
     INVALID_FLIGHT_ID("error.params", "The flight id provided is not valid, its length must be 8 characters"),
+    INVALID_RESERVATION_ID("error.params", "The reservation id provided is not valid, its length must be 6 characters"),
+    INVALID_RESERVATION_BODY("error.fields", "The reservation provided is not valid"),
     DATABASE_INTERNAL_ERROR("internal.error", "There was an error at operating with the database");
 
     override fun getCode(): String = code
