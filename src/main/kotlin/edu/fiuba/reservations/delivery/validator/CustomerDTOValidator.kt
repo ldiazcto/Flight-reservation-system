cde584916ebb27b0df7d7536f245bfa30262556e
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-12-11 18:27:12 -0300

    Remove required fields from childs to create reservation's DTO

--- src/main/kotlin/edu/fiuba/reservations/delivery/validator/CustomerDTOValidator.kt
+++ src/main/kotlin/edu/fiuba/reservations/delivery/validator/CustomerDTOValidator.kt
@@ -4,20 +4,14 @@ import edu.fiuba.reservations.delivery.dto.response.CustomerDTO
 import edu.fiuba.reservations.delivery.dto.response.DocumentDTO
 import edu.fiuba.reservations.delivery.dto.response.PhoneDTO
 import edu.fiuba.reservations.utils.enums.Exception
-import edu.fiuba.reservations.utils.generateRequiredFieldException
 import edu.fiuba.reservations.utils.ifNotNull
 import edu.fiuba.reservations.utils.ifNotNullAndBlank
-import edu.fiuba.reservations.utils.isNull
 import edu.fiuba.reservations.utils.validator.CompositeValidator
 import edu.fiuba.reservations.utils.validator.Validator
 
 class CustomerDTOValidator : BaseValidator(), Validator<CustomerDTO> {
     override fun validate(data: CustomerDTO, fieldNames: List<String>): List<Exception> {
         val exceptions = ArrayList<Exception>()
-
-        exceptions.addAll(
-            validateRequiredFields(data, fieldNames)
-        )
 
         data.documents?.forEach {
             setDocumentExceptions(
@@ -41,42 +35,6 @@ class CustomerDTOValidator : BaseValidator(), Validator<CustomerDTO> {
                 data = it,
                 fieldNames = fieldNames.plus(CustomerDTO::phone.name),
                 exceptions = exceptions
-            )
-        }
-
-        return exceptions
-    }
-
-    private fun validateRequiredFields(data: CustomerDTO, fieldNames: List<String>): List<Exception> {
-        val exceptions = ArrayList<Exception>()
-
-        if (data.firstName.isNullOrBlank()) {
-            exceptions.add(
-                generateRequiredFieldException(fieldNames.plus(CustomerDTO::firstName.name))
-            )
-        }
-
-        if (data.lastName.isNullOrBlank()) {
-            exceptions.add(
-                generateRequiredFieldException(fieldNames.plus(CustomerDTO::lastName.name))
-            )
-        }
-
-        if (data.documents.isNullOrEmpty()) {
-            exceptions.add(
-                generateRequiredFieldException(fieldNames.plus(CustomerDTO::documents.name))
-            )
-        }
-
-        if (data.email.isNullOrBlank()) {
-            exceptions.add(
-                generateRequiredFieldException(fieldNames.plus(CustomerDTO::email.name))
-            )
-        }
-
-        if (data.phone.isNull()) {
-            exceptions.add(
-                generateRequiredFieldException(fieldNames.plus(CustomerDTO::phone.name))
             )
         }
 
--- src/main/kotlin/edu/fiuba/reservations/delivery/validator/PhoneDTOValidator.kt
+++ src/main/kotlin/edu/fiuba/reservations/delivery/validator/PhoneDTOValidator.kt
@@ -3,17 +3,12 @@ package edu.fiuba.reservations.delivery.validator
 import edu.fiuba.reservations.delivery.dto.response.PhoneDTO
 import edu.fiuba.reservations.domain.enums.PhoneType
 import edu.fiuba.reservations.utils.enums.Exception
-import edu.fiuba.reservations.utils.generateRequiredFieldException
 import edu.fiuba.reservations.utils.ifNotNullAndBlank
 import edu.fiuba.reservations.utils.validator.Validator
 
 class PhoneDTOValidator : BaseValidator(), Validator<PhoneDTO> {
     override fun validate(data: PhoneDTO, fieldNames: List<String>): List<Exception> {
         val exceptions = ArrayList<Exception>()
-
-        exceptions.addAll(
-            validateRequiredFields(data, fieldNames)
-        )
 
         data.type.ifNotNullAndBlank {
             exceptions.addAll(
@@ -39,30 +34,6 @@ class PhoneDTOValidator : BaseValidator(), Validator<PhoneDTO> {
                     data = it,
                     fieldNames = fieldNames.plus(PhoneDTO::number.name)
                 )
-            )
-        }
-
-        return exceptions
-    }
-
-    private fun validateRequiredFields(data: PhoneDTO, fieldNames: List<String>): List<Exception> {
-        val exceptions = ArrayList<Exception>()
-
-        if (data.type.isNullOrBlank()) {
-            exceptions.add(
-                generateRequiredFieldException(fieldNames.plus(PhoneDTO::type.name))
-            )
-        }
-
-        if (data.areaCode.isNullOrBlank()) {
-            exceptions.add(
-                generateRequiredFieldException(fieldNames.plus(PhoneDTO::areaCode.name))
-            )
-        }
-
-        if (data.number.isNullOrBlank()) {
-            exceptions.add(
-                generateRequiredFieldException(fieldNames.plus(PhoneDTO::number.name))
             )
         }
 
